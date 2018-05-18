f6a141cd6449b54e28910a7f2f3cb857a6a91b46
Shawn Stewart <s.stewart0220@gmail.com>
2018-05-18 14:42:28 -0500

    did some stuff

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -5,6 +5,19 @@ const createUser = (req, res) => {
   const { username, password } = req.body;
   // create user takes in the username and password and saves a user.
   // our pre save hook should kick in here saving this user to the DB with an encrypted password.
+  if (username && password) {
+    const newUser = new User(req.body);
+    newUser
+      .save()
+      .then(response => {
+        res.status(201).json({ user: response });
+      })
+      .catch(err => {
+        res.status(500).json({ err: 'Error creating the new user.' });
+      });
+  } else {
+    res.status(500).json({ err: 'You must provide a username and password.' });
+  }
 };
 
 module.exports = {
